02421d2a6a48ca931bc3a8cf457131f96dc6ec8f
Saba Sarwar <sabasarwar1997@gmail.com>
2020-07-17 23:16:39 +0530

    even no of digits:method 02

--- Array/02_Find_Numbers_with_Even_Number_of_Digits/main.cpp
+++ Array/02_Find_Numbers_with_Even_Number_of_Digits/main.cpp
@@ -17,6 +17,8 @@ Therefore only 12 and 7896 contain an even number of digits.
 
 -------------------------------------------------------------------------------------------------*/
 
+//Solution 01
+
 class Solution 
 {
 public:
@@ -48,3 +50,29 @@ public:
     }
 };
 
+
+//Solution 02
+
+class Solution 
+{
+public:
+    int iseven(int a)
+    {
+        int c=0;
+        while(a!=0){
+            a=a/10;
+            c++;
+        }
+        if(c%2==0)
+            return 1;
+        else return 0;
+        
+    }
+    int findNumbers(vector<int>& nums) {
+        int c=0,i;
+        for(i=0;i<nums.size();i++)
+            if(iseven(nums[i]))
+                c++;
+        return c;
+    }
+};
