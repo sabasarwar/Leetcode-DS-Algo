06e94845f0530d968b0f1049dcb9ad37a11e4dac
Saba Sarwar <sabasarwar1997@gmail.com>
2020-08-25 23:53:11 +0530

    duplicate zeros: using stl

--- Array/04_Duplicate_Zeros/main.cpp
+++ Array/04_Duplicate_Zeros/main.cpp
@@ -39,3 +39,26 @@ public:
         }
     }
 };
+
+//Using STL
+
+class Solution 
+{
+public:
+    void duplicateZeros(vector<int>& arr) 
+    {
+
+        int i,j;
+        int n=arr.size();
+
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                arr.insert(arr.begin()+i, 0);
+                arr.pop_back();
+                i++;
+            }
+        }
+    }
+};
