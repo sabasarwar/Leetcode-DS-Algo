27c5f40317b51be948a4973dd2e0f65587412d27
Saba Sarwar <sabasarwar1997@gmail.com>
2020-08-26 23:34:01 +0530

    remove elements:simpler solution

--- Array/06_Remove_Elements/main.cpp
+++ Array/06_Remove_Elements/main.cpp
@@ -45,3 +45,22 @@ public:
     }
 };
 
+//Solution 02
+
+class Solution 
+{
+public:
+    int removeElement(vector<int>& nums, int val) 
+    {
+       for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == val)
+            {
+                nums.erase(nums.begin() + i);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
+
