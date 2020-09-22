0c6d7ce8172a605ceea4f099105c745c1fea0e01
Saba Sarwar <sabasarwar1997@gmail.com>
2020-09-22 21:07:39 +0530

    easy:mountain array

---
+++ Array/09_Valid_Mountain_Array/main.cpp
@@ -0,0 +1,65 @@
+/* ---------------------------------------- Problem Statement ------------------------------------
+Given an array A of integers, return true if and only if it is a valid mountain array.
+
+Recall that A is a mountain array if and only if:
+
+A.length >= 3
+There exists some i with 0 < i < A.length - 1 such that:
+A[0] < A[1] < ... A[i-1] < A[i]
+A[i] > A[i+1] > ... > A[A.length - 1]
+
+Example 1:
+
+Input: [2,1]
+Output: false
+Example 2:
+
+Input: [3,5,5]
+Output: false
+Example 3:
+
+Input: [0,3,2,1]
+Output: true
+-------------------------------------------------------------------------------------------------*/
+
+class Solution 
+{
+public:
+    bool validMountainArray(vector<int>& A) 
+    {
+        if(A.size() < 3)
+        {
+            return false;
+        }
+        
+        int i = 0;
+        bool inc = false, dec = false;
+        
+        while(i < A.size()-1)
+        {
+            if(A[i] < A[i+1])
+            {
+                i++;
+                inc = true;
+            }
+            else
+            {
+                break;
+            }
+        }
+       
+        while(i < A.size()-1)
+        {
+            if(A[i] > A[i+1])
+            {
+                dec = true;
+                i++;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return inc && dec;
+    }
+};
